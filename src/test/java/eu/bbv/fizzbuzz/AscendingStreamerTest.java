9e5bc62c58fd64939a04ce822192bc78b8487f18
Sebastian Hans <sebastian.hans@bbv.eu>
2025-02-14 14:10:08 +0100

    Introduce overlapping tests (failing)

--- src/test/java/eu/bbv/fizzbuzz/AscendingStreamerTest.java
+++ src/test/java/eu/bbv/fizzbuzz/AscendingStreamerTest.java
@@ -1,36 +1,23 @@
 package eu.bbv.fizzbuzz;
 
+import eu.bbv.fizzbuzz.math.Does;
 import eu.bbv.fizzbuzz.output.NumberOutput;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import static eu.bbv.fizzbuzz.output.FizzBuzzOutput.FIZZ;
+import static eu.bbv.fizzbuzz.output.FizzBuzzOutput.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class AscendingStreamerTest {
-    @Mock
-    private Selector selector;
-
-    private Streamer streamer;
-
-    @BeforeEach
-    void setUp() {
-        streamer = new AscendingStreamer(selector);
-    }
-
     @Test
     void returnsSelectedOutputsInOrder() {
-        when(selector.select(1)).thenReturn(new NumberOutput(1));
-        when(selector.select(2)).thenReturn(new NumberOutput(2));
-        when(selector.select(3)).thenReturn(FIZZ);
+        var streamer = new AscendingStreamer(new SimpleSelector(new Does()));
 
-        var stream = streamer.go(3);
+        var stream = streamer.go(15);
 
-        assertThat(stream).containsExactly(new NumberOutput(1), new NumberOutput(2), FIZZ);
+        assertThat(stream).containsExactly(
+            n(1), n(2), FIZZ, n(4), BUZZ, FIZZ, n(7), n(8), FIZZ, BUZZ, n(11), FIZZ, n(13), n(14), FIZZBUZZ
+        );
     }
+
+    private NumberOutput n(int n) { return new NumberOutput(n); }
 }
--- src/test/java/eu/bbv/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/eu/bbv/fizzbuzz/FizzBuzzTest.java
@@ -1,40 +1,44 @@
 package eu.bbv.fizzbuzz;
 
-import eu.bbv.fizzbuzz.output.NumberOutput;
+import eu.bbv.fizzbuzz.math.Does;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.stream.Stream;
-
-import static eu.bbv.fizzbuzz.output.FizzBuzzOutput.FIZZ;
-import static eu.bbv.fizzbuzz.output.FizzBuzzOutput.FIZZBUZZ;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FizzBuzzTest {
     @Mock
-    private Streamer streamer;
+    private Does does;
 
     private FizzBuzz fizzBuzz;
 
     @BeforeEach
     void setUp() {
-        fizzBuzz = new FizzBuzz(streamer);
+        fizzBuzz = new FizzBuzz(new AscendingStreamer(new SimpleSelector(does)));
     }
 
     @Test
     void aggregates() {
-        when(streamer.go(1)).thenReturn(Stream.of(new NumberOutput(1), new NumberOutput(2), FIZZ));
-        assertThat(fizzBuzz.go(1)).containsExactly("1", "2", "Fizz");
+        when(does.divide(3, 1)).thenReturn(false);
+        when(does.divide(5, 1)).thenReturn(false);
+        when(does.divide(3, 2)).thenReturn(false);
+        when(does.divide(5, 2)).thenReturn(false);
+        when(does.divide(3, 3)).thenReturn(true);
+        when(does.divide(5, 3)).thenReturn(false);
+
+        assertThat(fizzBuzz.go(3)).containsExactly("1", "2", "Fizz");
     }
 
     @Test
     void shoutsFIZZBUZZ() {
-        when(streamer.go(1)).thenReturn(Stream.of(FIZZBUZZ));
+        when(does.divide(3, 1)).thenReturn(true);
+        when(does.divide(5, 1)).thenReturn(true);
+
         assertThat(fizzBuzz.go(1)).containsExactly("FIZZBUZZ");
     }
 }
