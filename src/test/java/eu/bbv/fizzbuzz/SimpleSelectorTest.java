2fa9e10b01d8c6c18b1b046b0ae05e535f65d1c0
Sebastian Hans <sebastian.hans@bbv.eu>
2025-02-14 14:10:08 +0100

    Introduce overlapping tests

--- src/test/java/eu/bbv/fizzbuzz/SimpleSelectorTest.java
+++ src/test/java/eu/bbv/fizzbuzz/SimpleSelectorTest.java
@@ -2,53 +2,31 @@ package eu.bbv.fizzbuzz;
 
 import eu.bbv.fizzbuzz.math.Does;
 import eu.bbv.fizzbuzz.output.NumberOutput;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static eu.bbv.fizzbuzz.output.FizzBuzzOutput.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class SimpleSelectorTest {
-    @Mock
-    private Does does;
-
-    private SimpleSelector selector;
-
-    @BeforeEach
-    void setUp() {
-        selector = new SimpleSelector(does);
-    }
+    private final SimpleSelector selector = new SimpleSelector(new Does());
 
     @Test
-    void modNothingYieldsNumber() {
-        when(does.divide(3, 1)).thenReturn(false);
-        when(does.divide(5, 1)).thenReturn(false);
+    void oneIs1() {
         assertThat(selector.select(1)).isEqualTo(new NumberOutput(1));
     }
 
     @Test
-    void mod3YieldsFizz() {
-        when(does.divide(3, 1)).thenReturn(true);
-        when(does.divide(5, 1)).thenReturn(false);
-        assertThat(selector.select(1)).isEqualTo(FIZZ);
+    void threeIsFizz() {
+        assertThat(selector.select(3)).isEqualTo(FIZZ);
     }
 
     @Test
-    void mod5YieldsBuzz() {
-        when(does.divide(3, 1)).thenReturn(false);
-        when(does.divide(5, 1)).thenReturn(true);
-        assertThat(selector.select(1)).isEqualTo(BUZZ);
+    void fiveIsBuzz() {
+        assertThat(selector.select(5)).isEqualTo(BUZZ);
     }
 
     @Test
-    void mod3AndMod5YieldsFizzBuzz() {
-        when(does.divide(3, 1)).thenReturn(true);
-        when(does.divide(5, 1)).thenReturn(true);
-        assertThat(selector.select(1)).isEqualTo(FIZZBUZZ);
+    void fifteenIsFizzBuzz() {
+        assertThat(selector.select(15)).isEqualTo(FIZZBUZZ);
     }
 }
